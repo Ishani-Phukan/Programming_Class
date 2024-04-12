ac3dca917c89d81c1b0720e1b0cb99086ba50250
Ishani Phukan <Ishani.Phukan28@gmail.com>
2024-04-12 12:02:16 +0530

    Added center star pyramid

---
+++ Center_Star_Pyramid.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i,j,rows;
+    printf("Enter the number of rows:\n");
+    scanf("%d", &rows);
+    printf("Displaying the Center star pyramid:\n");
+    for(i=0;i<rows;i++){
+        for(j=0;j<rows-i;j++){
+            printf(" ");
+        }
+        for(j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
