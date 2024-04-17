9fd63d862d8a5e080af18252a0a574bdfc2f15c3
Ishani Phukan <Ishani.Phukan28@gmail.com>
2024-04-17 10:02:08 +0530

    Extracting Numbers

---
+++ Extracting_No.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int num, digit;
+
+  printf("Enter an integer: ");
+  scanf("%d", &num);
+
+  while (num > 0) {
+    digit = num % 10;
+    printf("%d ", digit);
+    num /= 10;
+  }
+
+  printf("\n");
+
+  return 0;
+}
