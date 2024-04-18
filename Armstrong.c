74c15fffdb77d0018e8035f6f75bac889390486e
Ishani Phukan <Ishani.Phukan28@gmail.com>
2024-04-18 21:47:09 +0530

    Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  int num, digit,temp,sum=0;
+
+  printf("Enter an integer: ");
+  scanf("%d", &num);
+
+    temp=num;
+  while (temp > 0) {
+    digit = temp % 10;
+    sum=sum+(digit*digit*digit);
+    temp /= 10;
+  }
+  if(sum==num){
+    printf("%d is an Armstrong Number", num);
+  }
+
+  else{
+    printf("%d is not an Armstrong Number", num);
+  }
+
+  printf("\n");
+
+  return 0;
+}
